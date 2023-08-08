65a4ac74b610e8798428007ffbb47be2daaf93dc
TheRealVVD <therealvvd@gmail.com>
2023-08-08 23:57:08 +0800

    Created logic for sensor.

--- src/main/java/api/project/rest/controllers/CheckController.java
+++
@@ -1,4 +0,0 @@
-package api.project.rest.controllers;
-
-public class CheckController {
-}
---
+++ src/main/java/api/project/rest/models/Sensor.java
@@ -0,0 +1,36 @@
+package api.project.rest.models;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "Sensor")
+public class Sensor implements Serializable { // Serializable для того, чтобы в Measurement можно было сделать FK из поля, не являющемся PK в Sensor (из поля name)
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name")
+    @NotEmpty(message = "Название не должно быть пустым!")
+    @Size(min = 3, max = 30, message = "Название сенсора должно быть от 3 до 30 символов!")
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
